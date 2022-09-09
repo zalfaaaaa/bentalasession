91b8ffdb36faeb439ddf5942653fb3472b0f50dd
ZALFA <110007888+zalfaaaaa@users.noreply.github.com>
2022-09-09 07:59:08 +0700

    delete delete.php

--- loginz/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$id=$_GET['id'];
-
-$d = new PDO('mysql:host=localhost;dbname=homesick','root',''); 
-$query = $d->query("delete from `usertb` where `id`='$id' ");
-
-if($query){
-    header('location:crud.php');}
